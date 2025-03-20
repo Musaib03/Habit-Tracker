d6ee827ae69ac6951e3c898ac3b36390aacb2cd5
Musaib03 <wanimusaib520@gmail.com>
2025-03-21 01:07:04 +0530

    salah routes updation and logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resource :session, only: [ :new, :create, :destroy ]
+  resource :session, only: [:new, :create, :destroy]
+  resources :salahs, only: [:new, :create, :index]
 
   # Root path should direct to the login page
   root to: "sessions#new"
-  # Health check for monitoring (optional)
+  get "/session", to: redirect("/session/new")
+  # Health check (optional)
   get "up" => "rails/health#show", as: :rails_health_check
-  # Uncomment if you want to enable PWA features
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
