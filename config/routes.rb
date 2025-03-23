77413f80d9fbaee16d0f2a1d98957e945ae37beb
Vinit Choudhary <vinitchahal1241@gmail.com>
2025-03-24 01:03:47 +0530

    index route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resource :session, only: [ :new, :create, :destroy ]
-  resources :salahs, only: [ :new, :create, :index ]
+  resources :salahs, only: [ :new, :create ]
 
   # Root path should direct to the login page
   root to: "sessions#new"
   get "/session", to: redirect("/session/new")
+  get "/salahs/index", to: "salahs#index"
   # Health check (optional)
   get "up" => "rails/health#show", as: :rails_health_check
 end
