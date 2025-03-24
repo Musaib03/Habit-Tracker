d8a8dcb0e2aaa0db7bc9cb16c665ccc90b6c2990
Vinit Choudhary <vinitchahal1241@gmail.com>
2025-03-24 22:55:47 +0530

    Salah Path Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resource :session, only: [ :new, :create, :destroy ]
-  resources :salahs, only: [ :new, :create ]
+  resources :salahs, only: [ :new, :create, :index]
 
   # Root path should direct to the login page
   root to: "sessions#new"
   get "/session", to: redirect("/session/new")
-  get "/salahs/index", to: "salahs#index"
   # Health check (optional)
   get "up" => "rails/health#show", as: :rails_health_check
 end
